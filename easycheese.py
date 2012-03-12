fb07d46e06301c6f7519cdcadcaffdb846e361c4
johnsm89@johnsm89-pro.swisscom.com <johnsm89@johnsm89-pro.swisscom.com>
2012-03-12 14:03:29 -0700

    pep8

--- easycheese.py
+++ easycheese.py
@@ -6,11 +6,14 @@ from bottle import get, post, static_file, request, route, run, view
 from bottle import jinja2_template as template
 
 bottle.debug()
-bottle.TEMPLATE_PATH.append(os.path.join(os.path.dirname(__file__), 'templates'))
+bottle.TEMPLATE_PATH.append(os.path.join(os.path.dirname(__file__),
+                                         'templates'))
+
 
 @get('/')
 def display_form():
     return template('form.html')
+
 
 @post('/')
 @view('setupdotpy_out')
@@ -42,13 +45,16 @@ def process_form():
         'packages': format_apps(packages),
     }
 
+
 @route('/download/setup.py')
 def download():
     return static_file('setup\.py', download='setup.py')
 
+
 @route('/static/:filename#.*(\.js|\.css)#')
 def static_serve(filename):
     return static_file(filename, root='static')
+
 
 def application(environ, start_response):
     return bottle.app()(environ, start_response)
